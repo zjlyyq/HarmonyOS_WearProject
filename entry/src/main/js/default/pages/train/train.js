63da7ac01cc103887dac2197af6d02e7a6f87cb3
zjl <jialuzhang0805@gmail.com>
2020-09-19 00:54:34 +0800

    实现倒计时，百分比等

--- entry/src/main/js/default/pages/train/train.js
+++ entry/src/main/js/default/pages/train/train.js
@@ -1,6 +1,29 @@
 import router from '@system.router'
+let speedSpan = 2;
+let time1 = null;
+let time2 = null;
+let timeout = 0;
+let startTime = 0;
+let totalTime = 0;
 export default {
+    data: {
+        total: 120,
+        interval: null,
+        isShow: true,
+        huxi: '吸气',
+        percent: 10
+    },
     clickAction() {
+        if (this.interval) {
+            console.log('interval id:' + this.interval);
+            clearInterval(this.interval);
+            this.interval = null;
+        }
+        if (time1) {
+            clearInterval(time1);
+            clearInterval(time2);
+            time2 = time1 = null;
+        }
         router.replace({
             uri: "pages/index/index"
         })
@@ -8,5 +31,47 @@ export default {
 
     onInit() {
         console.log(this.data1 + " " + this.data2);
+        this.total = 60 * this.data1;
+        speedSpan = this.data2 === '激烈'? 2:
+                this.data2 === '较快'? 4:
+                6;
+        totalTime = speedSpan * 1000;
+        this.run();
+        this.countdown();
+        this.interval = setInterval(() => {
+            if (this.total > 0) {
+                this.total -= 1;
+            }else {
+                clearInterval(this.interval);
+                if (time1) {
+                    clearInterval(time1);
+                    clearInterval(time2);
+                    time2 = time1 = null;
+                }
+                this.interval = null;
+                this.isShow = false;
+            }
+        }, 1000);
+        console.log('interval id:' + this.interval);
+    },
+    run() {
+        time1 = setInterval(() => {
+            if (this.huxi === '吸气') {
+                this.huxi = '呼气';
+            }else {
+                this.huxi = '吸气';
+            }
+            clearInterval(time2);
+            this.countdown();
+            this.percent = 0;
+        }, speedSpan*1000)
+    },
+
+    countdown() {
+        startTime = new Date().getTime();
+        time2 = setInterval(() => {
+            let now = new Date().getTime();
+            this.percent = Math.floor( (now-startTime) * 100 / totalTime ) ;
+        }, 100)
     }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 ### HarmonyOs development
 ![](./imgs/2020-09-15.png)
 当发生页面跳转，前一个页面被销毁
+
+tips:
+
+1. 文本大小只支持30px(默认)和38px
